450c437aef3458d96a4397cb444bf55d8cb12901
jordan aranda <xavieraranda123@gmail.com>
2023-06-27 15:03:00 -0500

    mis-proyectos

---
+++ study javascrit/js/codigo.js
@@ -0,0 +1,32 @@
+console.log("hola amigos");
+let numero = 10;
+
+console.log(numero)
+const solo = "yo";
+console.log(solo)
+let casa1, casa2, casa3, casa4;
+casa1 = "lopez";
+console.log(casa1)
+let a = 2, b = 5, resultado = 0;
+console.log(a)
+//ARIMETICA
+resultado = a + b;
+console.log(resultado)
+resultado = a - b;
+console.log(resultado)
+resultado = a * b;
+console.log(resultado)
+resultado = a / b;
+console.log(resultado)
+//MODULO O RESIDUO
+resultado = a % b;
+console.log(resultado)
+//POTENCIA (a=base y b = exponente)
+resultado = a ** b;
+console.log(resultado)
+
+//orden de presedencia--
+resultado= a+b * 5;
+console.log (resultado)
+resultado = (a+b*10)/34*(33*4)
+console.log(resultado)
